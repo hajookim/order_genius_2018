fffecc4ad20bce015a27a3dd049c86f091231ce4
Jason Swett <jason@benfranklinlabs.com>
2018-05-08 10:44:00 -0400

    Add seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create([
+  {
+    name: "Filthy J's Hot Dogs",
+    phone: '(111) 222-3333'
+  },
+  {
+    name: "Fast Freddies's Fantastically Fancy Fried Fish",
+    phone: '(558) 388-8338'
+  },
+  {
+    name: "Demonburger",
+    phone: '(946) 568-0011'
+  },
+  {
+    name: 'Taco Hole',
+    phone: '(404) 333-9338'
+  },
+  {
+    name: "Screamo's",
+    phone: '(944) 955-3983'
+  }
+])
